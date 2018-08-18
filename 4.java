f91c015338e741a41abee03db8a6332162d18735
Dipanjan Nayak <42298185+djnayak@users.noreply.github.com>
2018-08-18 12:31:20 +0530

    Update 4.java

--- 4.java
+++ 4.java
@@ -1,4 +1,9 @@
+/*
 
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+ */
  class prog_4 
 {
 	public static void main(String[] args) {
