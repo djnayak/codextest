a721b4c234c9b4a60c5318df2290f72ef13ffcf9
Dipanjan Nayak <42298185+djnayak@users.noreply.github.com>
2018-08-18 12:32:05 +0530

    Update 5.java

--- 5.java
+++ 5.java
@@ -1,4 +1,10 @@
- class Prog_5 {
+/*
+
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ */ 
+class Prog_5 {
 
         public static void main(String[] args) {
             System.out.println(findSmallestMultiple(10)); 
