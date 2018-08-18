88bc377ec17579e23756f073504ea1a960aec193
Dipanjan Nayak <42298185+djnayak@users.noreply.github.com>
2018-08-18 12:24:30 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -3,12 +3,13 @@
 {
 	public static void main (String[] args)
 	{
-		int size=1000;
+		int size=1000,sum=0;
 		int i;
 		for(i=0;i<size;i++)
 		{
 			if ((i % 3 == 0) || (i%5 == 0))
-			   System.out.println(i);
+			  sum=sum+i;
 		}
+		 System.out.println(sum);
 	}
 }
