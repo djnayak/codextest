48b6bb1cc723525a3608c01c59624aae2a980bec
Dipanjan Nayak <42298185+djnayak@users.noreply.github.com>
2018-08-18 12:18:21 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,11 +1,10 @@
-import java.util.Scanner;
+
  class prog_1 
 {
 	public static void main (String[] args)
 	{
-		int size;
+		int size=1000;
 		Scanner reader = new Scanner (System.in);
-		size = reader.nextInt();
 		int i;
 		for(i=0;i<size;i++)
 		{
