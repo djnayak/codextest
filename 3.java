2cf42d1ed75b2a9b20938ade10a6cf89949b6388
Dipanjan Nayak <42298185+djnayak@users.noreply.github.com>
2018-08-18 12:10:08 +0530

    Update 3.java

--- 3.java
+++ 3.java
@@ -1,29 +1,25 @@
 import java.util.Scanner;
  class prog_3 
 {
-	public static void main (String[] args)
-	{
-		int num;
-		Scanner reader = new Scanner (System.in);
-		num = reader.nextInt();
-		int i,j,max=0,c=0;
-		for(i=2;i<(num/2)+1;i++)
-		{
-			 if(num%i==0)
-			{
-				for(j=2;j<i;j++)
-				{
-					if ( i%j == 0)
-					{
-					   c++;	
-					}
-				}
-			if (c==0)
-			{
-				max=i;
-			}
-			 }
-		}
-		System.out.println("Largest Prime Factor = "+max);
+
+    public static void main(String[] args) {
+	 long NUMBER = 600851475143L;
+        long result = 0;
+        for(int i = 2; i < NUMBER; i++) {
+            if(NUMBER % i == 0 && isPrime(NUMBER / i)) {
+                result = NUMBER / i;
+                break;
+            }
+        }
+        System.out.println("Result: " + result);
+    }
+
+    private static boolean isPrime(long l) {
+        for(long num = 2, max = l / 2 ; num < max; num++) {
+            if(l % num == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 	}
-}
